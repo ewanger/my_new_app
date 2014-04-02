7b2f59d5e5799ed93d91ce5048b76e8841a8de31
Eric Wanger <eric@edw.com>
2014-04-02 13:14:53 -0500

    Let's override everything in refinery to see what's really there.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def footer_menu
+    menu_items = Refinery::Menu.new(Refinery::Page.footer_menu_pages)
+
+    presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+    presenter.dom_id = "footer_menu"
+    presenter.css = "footer_menu"
+    presenter.menu_tag = :div
+    presenter
+  end
+
+  def navigation_menu
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.css = "navbar-inner"
+    presenter.menu_tag = :div
+    presenter.list_tag_css = "nav"
+    presenter.selected_css = "active"
+    presenter.first_css = ""
+    presenter.last_css = ""
+    presenter.max_depth = 0 # prevents dropdown menus, which don't render correctly
+    presenter
+  end
+
 end
+
